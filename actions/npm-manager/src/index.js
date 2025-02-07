c2c959f42f8611f30e2b0507b2f872a488fd1a11
Pavel Anikin <pavel.anikin@netcracker.com>
2025-02-07 14:12:14 +0400

    feat: add npm-template workflow and update .gitignore for cleaner project structure

---
+++ actions/npm-manager/src/index.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const yaml = require('js-yaml');
+const core = require('@actions/core');
+// const { version } = require('os');
+
+
+// filePatch = './.github/npm-template.yml';
+
+let configDefault = {
+    version: 'latest',
+}
+
+
+async function loadConfig(filePatch) {
+
+    let config = { ...configDefault };
+
+    if (fs.existsSync(filePatch)) {
+        try {
+            const fileConfig = yaml.load(fs.readFileSync(filePatch, 'utf8'));
+            config = { ...config, ...fileConfig };
+        } catch (error) {
+            core.error(`Error loading config file: ${error}`);
+        }
+    }
+
+    config.version = core.getInput('version') || config.version;
+    return config;
+}
+
+
+
+async function run() {
+
+    let filePatch = core.getInput('filePatch');
+    let result = await loadConfig(filePatch);
+    core.info(`Config: ${JSON.stringify(result)}`);
+}
+
+run();
