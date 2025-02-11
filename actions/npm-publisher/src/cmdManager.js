de64a9ca89c478b3c666f8f29f7e074273ccf1fa
Pavel Anikin <pavel.anikin@netcracker.com>
2025-02-12 00:58:33 +0400

    feat: enhance CmdManager to capture stdout and stderr for better error reporting

--- actions/npm-publisher/src/cmdManager.js
+++ actions/npm-publisher/src/cmdManager.js
@@ -9,11 +9,17 @@ class CmdManager {
                 ...process.env,
                 NODE_AUTH_TOKEN: process.env.NODE_AUTH_TOKEN || process.env.GITHUB_TOKEN,
             },
+            listeners: {
+                stdout: (data) => this.stdout += data.toString(),
+                stderr: (data) => this.stderr += data.toString(),
+            },
         };
         try {
             await exec.exec(command, args, options);
         } catch (error) {
             core.setFailed(`Error executing command: ${error}`);
+            error.stdout = this.stdout;
+            error.stderr = this.stderr;
             throw error;
         }
     }
