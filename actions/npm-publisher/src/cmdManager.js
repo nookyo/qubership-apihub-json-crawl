77675f6cab1106ceafc0eba4fdec026b07199989
Pavel Anikin <pavel.anikin@netcracker.com>
2025-02-12 01:03:30 +0400

    refactor: remove stdout and stderr listeners from CmdManager and simplify command execution

--- actions/npm-publisher/src/cmdManager.js
+++ actions/npm-publisher/src/cmdManager.js
@@ -10,17 +10,8 @@ class CmdManager {
                 ...process.env,
                 NODE_AUTH_TOKEN: process.env.NODE_AUTH_TOKEN || process.env.GITHUB_TOKEN,
             },
-            listeners: {
-                stdout: (data) => this.stdout += data.toString(),
-                stderr: (data) => this.stderr += data.toString(),
-            },
         };
-        try {
-            await exec.exec(command, args, options);
-        } catch (error) {
-            core.error(`Error executing command: ${error}`);
-            throw error;
-        }
+        await exec.exec(command, args, options);
     }
 }
 
