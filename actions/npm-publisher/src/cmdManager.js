2ebb51cf9b5c7c7c4ac22a3f28a64b1e0b378ee7
Pavel Anikin <pavel.anikin@netcracker.com>
2025-02-12 01:00:15 +0400

    feat: integrate core module for enhanced error handling in CmdManager

--- actions/npm-publisher/src/cmdManager.js
+++ actions/npm-publisher/src/cmdManager.js
@@ -1,3 +1,4 @@
+const core = require('@actions/core');
 const exec = require('@actions/exec');
 
 class CmdManager {
@@ -9,17 +10,10 @@ class CmdManager {
                 ...process.env,
                 NODE_AUTH_TOKEN: process.env.NODE_AUTH_TOKEN || process.env.GITHUB_TOKEN,
             },
-            listeners: {
-                stdout: (data) => this.stdout += data.toString(),
-                stderr: (data) => this.stderr += data.toString(),
-            },
         };
         try {
             await exec.exec(command, args, options);
         } catch (error) {
-            core.setFailed(`Error executing command: ${error}`);
-            error.stdout = this.stdout;
-            error.stderr = this.stderr;
             throw error;
         }
     }
